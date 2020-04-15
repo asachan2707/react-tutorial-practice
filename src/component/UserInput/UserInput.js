29eb229f13cbb0ef1a80b57a10f368ff9cef0527
asachan2707 <asachan2707@gmail.com>
2020-04-15 18:22:51 +0530

    Changes in project structure

--- src/component/UserInput/UserInput.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react';
-
-const userInput = (props) => {
-    return <input type="text" onChange={props.changed} value={props.currentName}/>
-}
-
-export default userInput;
