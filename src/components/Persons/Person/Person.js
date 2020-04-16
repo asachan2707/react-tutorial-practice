642adaa4bb9cb90c0ffcc121dacc77ce7707d682
asachan2707 <asachan2707@gmail.com>
2020-04-16 15:31:20 +0530

    Getting JSX element Reference.
    One way to directly use annonymous function as value in ref for any element.
    
    Else create ref through React.createRef() in constructor.

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -6,6 +6,15 @@ import withClass from '../../../hoc/WithClass'
 
 class Person extends Component {
 
+  constructor(props) {
+    super(props);
+    this.inputElement = React.createRef();
+  }
+
+  componentDidMount() {
+    this.inputElement.current.focus();
+  }
+
   render() {
     console.log('[Person.js] rendering...');
     return (
@@ -14,7 +23,12 @@ class Person extends Component {
           I'm {this.props.name} and I am {this.props.age} years old!
         </p>
         <p>{this.props.children}</p>
-        <input type="text" onChange={this.props.changed} value={this.props.name} />
+        <input
+          type="text"
+          // ref={(inputEl) => {this.inputElement = inputEl}}
+          ref={this.inputElement}
+          onChange={this.props.changed}
+          value={this.props.name} />
       </Aux>
     );
   }
