c48efd0485a70803eabc06cb56b1d82e95fb457a
asachan2707 <asachan2707@gmail.com>
2020-04-16 15:17:23 +0530

    Added types in props

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import classes from './Person.css';
 import Aux from '../../../hoc/Auxilliry';
 import withClass from '../../../hoc/WithClass'
@@ -18,4 +19,12 @@ class Person extends Component {
     );
   }
 }
+
+Person.propTypes = {
+  click: PropTypes.func,
+  name: PropTypes.string,
+  age: PropTypes.number,
+  changed: PropTypes.func
+};
+
 export default withClass(Person, classes.Person);
