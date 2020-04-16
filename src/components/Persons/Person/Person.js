4a1eee662e09acd4dad1111851db4abeb41efd76
asachan2707 <asachan2707@gmail.com>
2020-04-16 19:29:56 +0530

    Another way of getting Context in React
    For class based component:-
    We can add
          static contextType = AuthContext;
    and this.context will have all the props.
    
    For functional component:-
    We can use 'useContext' react hook and pass the context created by React.createContext in auth-context.

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -12,17 +12,18 @@ class Person extends Component {
     this.inputElement = React.createRef();
   }
 
+  static contextType = AuthContext;
+
   componentDidMount() {
     this.inputElement.current.focus();
+    console.log(this.context.authenticated);
   }
 
   render() {
     console.log('[Person.js] rendering...');
     return (
       <Aux>
-        <AuthContext.Consumer>
-          {(context) => context.authenticated ? 'Authenticated! :)' : 'Please Login.'}
-        </AuthContext.Consumer>
+        {this.context.authenticated ? 'Authenticated! :)' : 'Please Login.'}
         <p onClick={this.props.click}>
           I'm {this.props.name} and I am {this.props.age} years old!
         </p>
