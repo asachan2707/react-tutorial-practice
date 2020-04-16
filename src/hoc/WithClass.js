a3c093b057a03622b45e847201f129db8e6b9bc6
asachan2707 <asachan2707@gmail.com>
2020-04-16 13:42:56 +0530

    Passing props in HOC

--- src/hoc/WithClass.js
+++ src/hoc/WithClass.js
@@ -3,7 +3,7 @@ import React from 'react';
 const withClass = (WrapperComponent, classes) => {
     return props => {
         return <div className={classes}>
-            <WrapperComponent />
+            <WrapperComponent {...props}/>
         </div>
     }
 }
